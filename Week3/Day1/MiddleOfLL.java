3f885275cb0d0a80b8d2ba53daa0ff3e4b0c2941
Ishita Agarwal <ishita05agarwal@gmail.com>
2026-06-15 22:37:14 +0530

    updated code

---
+++ Week3/Day1/MiddleOfLL.java
@@ -0,0 +1,36 @@
+package Week3.Day1;
+
+public class MiddleOfLL {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        MiddleOfLL obj = new MiddleOfLL();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        ListNode middle = obj.middleNode(head);
+        System.out.println("Middle Node = " + middle.val);
+        
+    }
+
+}
