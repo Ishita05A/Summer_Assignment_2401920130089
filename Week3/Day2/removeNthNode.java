8c734673b1ad30328454953f0c1b283db0b5dac4
Ishita Agarwal <ishita05agarwal@gmail.com>
2026-06-16 23:27:27 +0530

    updated code

---
+++ Week3/Day2/removeNthNode.java
@@ -0,0 +1,53 @@
+package Week3.Day2;
+
+public class removeNthNode {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+        ListNode slow = temp;
+        ListNode fast = temp;
+        for (int i = 1; i <= n + 1; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        if (slow.next != null) {
+            slow.next = slow.next.next;
+        }
+        return temp.next;
+
+    }
+
+    public static void main(String[] args) {
+
+        removeNthNode obj = new removeNthNode();
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        int n = 2;
+
+        ListNode result = obj.removeNthFromEnd(head, n);
+
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+
+}
